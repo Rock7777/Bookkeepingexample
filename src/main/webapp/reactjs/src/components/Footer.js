f19c9c2e076182036336557f8387c66698a2d452
Rakesh7 <Rakesh7@users.noreply.github.com>
2020-06-04 16:50:35 +0530

    Heyy it is related to routing

---
+++ src/main/webapp/reactjs/src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Container,Navbar,Col } from 'react-bootstrap'
+
+class Footer extends React.Component
+{
+       render()
+       {
+       let fullYear = new Date().getFullYear()
+        return(
+           <Navbar fixed="bottom" bg="dark" variant="dark">
+               <Container>
+                 <Col lg={12} className="text-center text-muted">
+                 <div>
+                       {fullYear}-{fullYear+1},Heyy Hii from Footer
+                       </div>
+                  </Col>
+               </Container>
+           </Navbar>
+
+        )
+       }
+
+
+}
+
+export default Footer
